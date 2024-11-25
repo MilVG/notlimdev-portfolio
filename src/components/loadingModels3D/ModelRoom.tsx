b4f19ba12884a311aca927dd6286fd7eb5db4cfd
notlim_dev <elmotigre6@gmail.com>
2024-11-25 04:10:32 -0500

    Implementacion e Loading Models- 3D

--- src/components/loadingModels3D/ModelRoom.tsx
+++ src/components/loadingModels3D/ModelRoom.tsx
@@ -1,22 +1,10 @@
+import { Model } from "./Model001";
 
-import React from 'react'
-import { useGLTF } from '@react-three/drei'
-import { Group } from 'three';
-
-interface GLTFResult {
-  scene: Group; // Representa el objeto cargado
-  nodes: any; // Puedes ajustar esto si sabes los nodos específicos
-  materials: any; // Puedes ajustar esto si sabes los materiales específicos
-}
 
 export default function ModelRoom() {
 
-  const { scene } = useGLTF('/assets/model_3d_room/scene.gltf') as GLTFResult
-  scene.scale.set(20, 20, 20)
-  scene.position.set(0.7, -0.5, 0)
-  scene.rotation.set(0, 0.5, 0)
-
-  return (<primitive object={scene} />)
+  return (
+    <Model scale={[0.2, 0.2, 0.2]} position={[0.8, 0, 0]} rotation={[0, -0.8, 0]} />
+  )
 
 }
-useGLTF.preload('/assets/model_3d_room/scene.gltf')
