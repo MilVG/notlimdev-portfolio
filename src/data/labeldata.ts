b8c7a37886e45b80d80a052995c3e39c8e6bf204
notlim_dev <elmotigre6@gmail.com>
2024-12-17 22:59:19 -0500

    eliminacion de stylos dinamicos del array de datos de los labels

--- src/data/labeldata.ts
+++ src/data/labeldata.ts
@@ -2,7 +2,6 @@ export type LabelsDataTypes = {
   id: string
   title: string
   description: string
-  styles: string
 }
 
 export const labelsData: LabelsDataTypes[] = [
@@ -10,30 +9,25 @@ export const labelsData: LabelsDataTypes[] = [
     id: "label_01",
     title: "NotlimDev",
     description: "El Verdadero Trabajo de un profesional es asegurar la calidad de Software",
-    styles: "absolute"
   },
   {
     id: "label_02",
     title: "Evolución",
     description: "Me Apasiona estar actualizado con las Nuevas tecnologías",
-    styles: "absolute bottom-0 left-0"
   },
   {
     id: "label_03",
     title: "Creatividad",
     description: "El punto clave en la tecnología es aportar nuevas y valisosas ideas.",
-    styles: "absolute top-0 right-0"
   },
   {
     id: "label_04",
     title: "Equipo",
     description: "La riqueza de cada Persona, crece al escuchar y ser participe de su grupo de Trabajo.",
-    styles: "absolute bottom-0 right-0"
   },
   {
     id: "label_05",
     title: "Relajación",
-    description: "Despejar tu mente es saludable, Escoje lo que ma ste guste para Distraerte",
-    styles: "absolute  top-0 right-0"
+    description: "Despejar tu mente es saludable, Escoje lo que más te guste para Distraerte",
   }
 ]
