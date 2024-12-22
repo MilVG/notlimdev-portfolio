240deec317d0328c09294800e4a6ef413a6b36b4
notlim_dev <elmotigre6@gmail.com>
2024-12-21 23:29:38 -0500

    Agregando el segundo avance de la seccion de skills - Reorganizacion de Objetos 3D

--- src/components/portfolioPages/main/skills/SkillsScene.tsx
+++ src/components/portfolioPages/main/skills/SkillsScene.tsx
@@ -2,29 +2,62 @@
 
 import styles from "@/app/portfolio/root_portfolio.module.css";
 import * as THREE from "three"
-import { Decal, OrbitControls, useTexture } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import { Suspense, useState } from "react";
+import { Decal, useTexture } from "@react-three/drei";
+import { Canvas, ThreeEvent } from "@react-three/fiber";
+import { Suspense, useState, useRef } from "react";
 import DashedLineCurve from "./LineCurve";
 import { dataPathTexture, dataPathTextureT } from "@/data/DataPathImageTexture";
 
 interface textureMeshI extends Omit<dataPathTextureT, 'image' | 'id'> {
   texturePath: string
 }
-function TexturedMesh({ texturePath, positionMesh, positionDecal, rotation, scalemesh, scaleDecal }: textureMeshI) {
-  const texture = useTexture(texturePath)
 
+type handleEventThreT = (event: ThreeEvent<PointerEvent>) => void;
+
+function TexturedMesh({ texturePath, positionMesh, positionDecal, rotation, scalemesh, scaleDecal }: textureMeshI) {
+
+  const texture = useTexture(texturePath)
+  const meshRef = useRef<THREE.Mesh>(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [lastMousePosition, setLastMousePosition] = useState<{ x: number, y: number } | null>(null);
+
+  const handlePointerDown: handleEventThreT = (event) => {
+    setIsDragging(true);
+    setLastMousePosition({ x: event.clientX, y: event.clientY });
+  };
+
+  const handlePointerMove: handleEventThreT = (event) => {
+    if (!isDragging || !meshRef.current || !lastMousePosition) return;
+
+    const deltaX = event.clientX - lastMousePosition.x;
+    const deltaY = event.clientY - lastMousePosition.y;
+
+    meshRef.current.rotation.y += deltaX * 0.01; // Rotación en eje Y
+    meshRef.current.rotation.x += deltaY * 0.01; // Rotación en eje X
+
+    setLastMousePosition({ x: event.clientX, y: event.clientY });
+  };
+
+  const handlePointerUp = () => setIsDragging(false);
   return (
-    <mesh scale={scalemesh} rotation={rotation} position={positionMesh}>
-      <octahedronGeometry args={[1, 2]} />
-      <meshStandardMaterial color="#ffffff" flatShading />
-      <Decal
-        map={texture}
-        position={positionDecal}
-        rotation={[0, 0, 0]}
-        scale={scaleDecal}
-      />
-    </mesh>
+    <>
+      <mesh ref={meshRef} scale={scalemesh} rotation={rotation}
+        position={positionMesh}
+        onPointerDown={handlePointerDown}
+        onPointerMove={handlePointerMove}
+        onPointerUp={handlePointerUp}
+        onPointerOut={handlePointerUp} // Por si el puntero sale de la malla
+      >
+        <octahedronGeometry args={[1, 2]} />
+        <meshStandardMaterial color="white" flatShading />
+        <Decal
+          map={texture}
+          position={positionDecal}
+          rotation={[0, 0, 0]}
+          scale={scaleDecal}
+        />
+      </mesh>
+    </>
   );
 }
 
@@ -34,7 +67,7 @@ export default function SkillsScene() {
   return (
     <div className={styles.skills}>
       <Canvas>
-        <directionalLight intensity={0.2} position={[1, 6, 10]} />
+        <directionalLight intensity={1.7} position={[1, 6, 10]} />
         <ambientLight intensity={0.1} position={[1, 4, 4]} />
         <Suspense fallback={null}>
           {
@@ -46,7 +79,7 @@ export default function SkillsScene() {
                 positionMesh={texture.positionMesh}
                 rotation={texture.rotation}
                 scalemesh={texture.scalemesh}
-                scaleDecal={texture.scaleDecal ? 1.2 : texture.scaleDecal}
+                scaleDecal={texture.scaleDecal ? 1.5 : texture.scaleDecal}
               />
             ))
           }
@@ -80,7 +113,7 @@ export default function SkillsScene() {
                 new THREE.Vector2(1.5, 0),
 
               ]}
-              dashSize={0.2}
+              dashSize={0.5}
             />
             <DashedLineCurve
               controlPoints={[
@@ -89,11 +122,11 @@ export default function SkillsScene() {
                 new THREE.Vector2(1.5, -2),
                 new THREE.Vector2(1.5, 0)
               ]}
-              dashSize={0.2}
+              dashSize={0.5}
             />
           </group>
         </Suspense>
-        <OrbitControls enableZoom={false} />
+        {/*<OrbitControls enableZoom={false} />*/}
       </Canvas>
     </div>
   );
