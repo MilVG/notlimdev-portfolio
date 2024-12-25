f5762e6b06e61ae1ac24073628d063d9dc658150
notlim_dev <elmotigre6@gmail.com>
2024-12-24 20:41:00 -0500

    Creacion e Incorporacion de de seccion de projects - primer proceso

---
+++ src/components/sectionsPages/main/projects/Projects.tsx
@@ -0,0 +1,8 @@
+import styles from "@/components/sectionsPages/main/projects/styles-projects.module.css"
+export const Projects = () => {
+  return (
+    <div className={styles.projects}>
+      Projects</div>
+  )
+}
+
