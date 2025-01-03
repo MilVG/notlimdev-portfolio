ab05f35a2c373b1aa6b6670309059975d0455680
notlim_dev <elmotigre6@gmail.com>
2025-01-02 20:02:00 -0500

    Finalizacion primer porceso de seccion de projectos Interfaz

--- src/app/portfolio/layout.tsx
+++ src/app/portfolio/layout.tsx
@@ -1,15 +1,7 @@
-import { Roboto } from "next/font/google"
-
-const roboto = Roboto({
-  weight: ['400', '700', '900'],
-  style: ['normal'],
-  subsets: ['latin'],
-  display: 'swap'
-})
 export default function layoutHome({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <div className='h-screen w-full'>
-      <main className={`${roboto.className}`}>{children}</main>
+      <main>{children}</main>
 
     </div>
   )
--- src/components/sectionsPages/main/projects/Projects.tsx
+++ src/components/sectionsPages/main/projects/Projects.tsx
@@ -1,8 +1,61 @@
-import styles from "@/components/sectionsPages/main/projects/styles-projects.module.css"
+"use client"
+import { useEffect, useRef } from "react"
+import gsap from "gsap"
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
+import Lenis from "lenis";
+
+
 export const Projects = () => {
+
+  const horizotalPanelsRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const lenis = new Lenis();
+    lenis.on('scroll', () => { });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy(); // Limpiar cuando el componente se desmonta
+    };
+  }, []);
+
+
+
+  useGSAP(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    const sections = gsap.utils.toArray(".panel")
+
+    gsap.config({ force3D: true })
+    gsap.to('.panel', {
+      xPercent: -(100 * (sections.length - 1)),
+      ease: "none",
+      scrollTrigger: {
+        trigger: horizotalPanelsRef.current,
+        scrub: 1,
+        end: () => "+=" + horizotalPanelsRef.current?.offsetWidth,
+        pin: true,
+        snap: 1 / (sections.length - 1),
+      }
+    })
+
+  }, { scope: horizotalPanelsRef })
   return (
-    <div className={styles.projects}>
-      Projects</div>
+    <>
+      <section ref={horizotalPanelsRef} className="min-h-screen flex flex-nowrap items-center space-x-10 px-10">
+        <div className="w-1/2  h-[50vh] shrink-0 rounded-3xl bg-slate-600 panel will-change-transform"></div>
+        <div className="w-1/2 h-[50vh] shrink-0 rounded-3xl bg-purple-400 panel will-change-transform" ></div>
+        <div className="w-1/2 h-[50vh] shrink-0 rounded-3xl bg-green-600  panel will-change-transform" ></div>
+        <div className="w-1/2 h-[50vh] shrink-0 rounded-3xl bg-yellow-500 panel will-change-transform" ></div>
+      </section>
+
+    </>
   )
 }
 
