7bbdcf045d26fb3636ce335faa22c76ca6ca4a41
notlim_dev <elmotigre6@gmail.com>
2025-01-13 18:24:24 -0500

    Eliminacion y traslado de componentes

--- src/components/sectionsPages/main/head/Header.tsx
+++ src/components/sectionsPages/main/head/Header.tsx
@@ -1,36 +1,16 @@
 
-import React from 'react'
-import styles from "./elementsHeader/header.module.css"
-
-import stylesRoot from "@/app/portfolio/root_portfolio.module.css";
 import Banner from './elementsHeader/Banner';
 import HeaderNavigation from './elementsHeader/HeaderNavigation';
-import Presentation from './elementsHeader/Presentation';
-import AccessDownloadCv from './elementsHeader/AccessDownloadCv';
 export default function Header() {
 
 
   return (
-    <div className={stylesRoot.headdiv}>
-      <div className={styles.header_container}>
-        <div className={`relative h-full`}>
+    <div className="relative  w-full h-full flex flex-col justify-start items-center ">
+      {/*header Navigation*/}
+      <HeaderNavigation />
 
-          {/*header Navigation*/}
-          <HeaderNavigation />
-
-          {/*Banner_Background*/}
-          <Banner />
-
-          {/*Text - Presentation*/}
-          <Presentation />
-
-          {/*Text - CV -Download*/}
-          <AccessDownloadCv />
-        </div>
-        <div>
-
-        </div>
-      </div>
+      {/*Banner_Background*/}
+      <Banner />
     </div>
   )
 }
