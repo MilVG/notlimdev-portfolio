a6ffdc20abcb09b373eae65ec11a6f0893d0ba46
notlim_dev <elmotigre6@gmail.com>
2025-01-13 18:28:50 -0500

    Agregando la seccion de about scena para la restructuracion de stylos responsivos

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,18 +1,14 @@
 
 import styles from "@/app/portfolio/root_portfolio.module.css";
 import AboutScene from "@/components/sectionsPages/main/about/AboutScene";
-import { Contact } from "@/components/sectionsPages/main/contact/Contact";
-import { Experience } from "@/components/sectionsPages/main/experience/Experience";
 import Header from "@/components/sectionsPages/main/head/Header";
-import { Projects } from "@/components/sectionsPages/main/projects/Projects";
-import SkillsScene from "@/components/sectionsPages/main/skills/SkillsScene";
 
 export default function RootPage() {
 
   return (
     <div className={styles.rootpage}>
       <Header />
-      {/* <AboutScene /> */}
+      <AboutScene />
       {/* <SkillsScene /> */}
       {/* <Experience /> */}
       {/* <Projects /> */}
