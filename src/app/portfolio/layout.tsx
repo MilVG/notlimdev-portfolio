96ca4481c3849a14fca3ff2701c4ced0275856ac
notlim_dev <elmotigre6@gmail.com>
2025-01-24 17:39:12 -0500

    eliminando ruta innecesaria

--- src/app/portfolio/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function layoutHome({ children }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <div className='relative h-full w-full'>
-      {children}
-    </div>
-  )
-}
-
