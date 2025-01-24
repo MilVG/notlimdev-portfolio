76037fa8345277ccc9daf0ca9c78b279fcc664b5
notlim_dev <elmotigre6@gmail.com>
2025-01-24 17:39:56 -0500

    Incorporando el codigo de los compoenentes principales

---
+++ src/components/sectionsPages/main/Home.tsx
@@ -0,0 +1,21 @@
+import AboutScene from "@/components/sectionsPages/main/about/AboutScene";
+import { Experience } from "@/components/sectionsPages/main/experience/Experience";
+import Header from "@/components/sectionsPages/main/head/Header";
+import { Projects } from "@/components/sectionsPages/main/projects/Projects";
+import SkillsScene from "@/components/sectionsPages/main/skills/SkillsScene";
+
+export default function Home() {
+
+  return (
+    <div className="w-full h-screen">
+      <Header />
+      <div className="relative w-full h-[500%]">
+        <AboutScene />
+      </div>
+      <SkillsScene />
+      <Experience />
+      <Projects />
+      {/* <Contact /> */}
+    </div>
+  );
+}
