30b5ce05e063392c243be6643c20dce9eb17bd54
notlim_dev <elmotigre6@gmail.com>
2025-01-27 22:12:34 -0500

    incorporacion y asignacion de estado global para el div principal de la seccion

--- src/components/sectionsPages/main/experience/Experience.tsx
+++ src/components/sectionsPages/main/experience/Experience.tsx
@@ -1,9 +1,13 @@
 "use client"
 
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { TimelineLogo } from "./elements/TimelineLogo";
+import { useDivRefsStore } from '@/store/store-sections';
 
 export const Experience = () => {
+
+  const sectionExperience = useRef<HTMLDivElement>(null)
+  const setDivRef = useDivRefsStore((state) => state.setDivRef)
 
   const [fechas] = useState([
     "Febrero-2022",
@@ -34,8 +38,14 @@ export const Experience = () => {
       });
   }
   const dates = getSortedDates()
+
+  useEffect(() => {
+    if (sectionExperience.current) {
+      setDivRef('Experience', sectionExperience)
+    }
+  }, [sectionExperience, setDivRef])
   return (
-    <div className="w-full h-[220%] relative max-sm:h-[160%] top-10">
+    <div id='Experience' className="w-full h-[220%] relative max-sm:h-[160%] top-10" ref={sectionExperience}>
       <div>
         <div className="absolute max-sm:h-[89%] h-[90%] w-full max-sm:top-0 sm:top-28 flex flex-row justify-center items-center max-sm:justify-end max-sm:p-10">
           <div className={`w-1 h-full border-2 border-slate-600 `}></div>
